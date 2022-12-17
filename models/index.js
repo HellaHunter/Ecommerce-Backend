fa80f90d684dd428dbba8885c7f1f7879794af78
Hunter O'Neal <hunteroneal54@gmail.com>
2022-12-17 14:36:35 -0600

    Finalized everything

--- models/index.js
+++ models/index.js
@@ -4,20 +4,20 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-Product.belongsTo(Category, {
-  foreignKey: 'category_id'
-});
+Product.belongsTo(Category, 
+  {foreignKey: 'category_id'},
+  {onDelete: 'cascade'}
+);
 
 Category.hasMany(Product, {
   foreignKey: 'category_id',
 });
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, {
-  through: {
-    model: ProductTag
-  },
-});
+Product.belongsToMany(Tag, 
+  { through: ProductTag }, 
+  { onDelete: "cascade" }
+);
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -33,10 +33,10 @@ router.get('/:id', (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
   try {
-    const tagData = Tag.create(req.body);
+    const tagData = await Tag.create(req.body);
     res.status(200).json(tagData)
   } catch(err) {
     res.status(500).json(err)
