3e341974857b26a0b4c9367c2e5dbda8e8ced9c1
Hunter O'Neal <hunteroneal54@gmail.com>
2022-12-17 11:15:46 -0600

    set up category-routes

--- models/index.js
+++ models/index.js
@@ -4,9 +4,14 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-Product.belongsTo(Category);
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
-Category.haveMany(Product);
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongToMany(Tag, {
@@ -14,6 +19,7 @@ Product.belongToMany(Tag, {
     model: ProductTag,
     unique: false
   },
+  as: 'product_prices'
 });
 
 // Tags belongToMany Products (through ProductTag)
@@ -22,6 +28,7 @@ Tag.belongToMany(Product, {
     model: ProductTag,
     unique: false
   },
+  as: 'product_tags'
 });
 
 module.exports = {
